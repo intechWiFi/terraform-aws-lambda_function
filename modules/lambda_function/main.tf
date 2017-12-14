afc100cbd25fbda65065e60a8beed9e674e45300
Robin Bowes <robin.bowes@yo61.com>
2017-12-14 23:51:37 +0000

    Add vpc_config, improve variable documentation in lambda sub-module

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -4,8 +4,12 @@ Create the lambda function
 resource "aws_lambda_function" "function" {
   filename         = "${var.filename}"
   function_name    = "${var.function_name}"
-  role             = "${var.role}"
   handler          = "${var.handler}"
-  source_code_hash = "${base64sha256(file("${var.filename}"))}"
+  role             = "${var.role}"
   runtime          = "${var.runtime}"
+  source_code_hash = "${base64sha256(file("${var.filename}"))}"
+  vpc_config       = {
+    security_group_ids = ["${var.security_group_ids}"]
+    subnet_ids         = ["${var.subnet_ids}"]
+  }
 }
